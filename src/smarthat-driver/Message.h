077618348d42360ed2af7554acdb7be8a2ffb232
Elle Goodarzi <Ellegoodarzi@gmail.com>
2025-03-14 20:59:56 -0400

    Update Message.h

--- src/smarthat-driver/Message.h
+++ src/smarthat-driver/Message.h
@@ -1,28 +1,28 @@
 #ifndef SMARTHAT_HARDWARE_MESSAGE_H
 #define SMARTHAT_HARDWARE_MESSAGE_H
 
-#include <string>
+#include <Arduino.h>
 #include <ArduinoJson.h>
 
 class Message {
     public:
        
         Message();
-        Message(std::string messageType, float data);
+        Message(const char* messageType, float data);
         
      
-        std::string getJsonMessage();
+        String getJsonMessage();
         
      
-        static const std::string DUST_SENSOR_DATA;
-        static const std::string SOUND_SENSOR_DATA;
+        static const char* DUST_SENSOR_DATA;
+        static const char* SOUND_SENSOR_DATA;
        
-        std::string getMessageType() const { return messageType; }
+        const char* getMessageType() const { return messageType; }
         float getData() const { return data; }
         unsigned long getTimeStamp() const { return timeStamp; }
 
     private:
-        const std::string messageType;
+        const char* messageType;
         const float data;
         const unsigned long timeStamp;
 };
