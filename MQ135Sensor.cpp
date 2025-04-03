60020ddf41924c7312bef7c12b7f09aa1be02976
4050hop <147534729+4050hop@users.noreply.github.com>
2025-04-03 17:41:04 -0400

    Add files via upload

---
+++ MQ135Sensor.cpp
@@ -0,0 +1,59 @@
+#include "MQ135Sensor.h"
+#include <Arduino.h>
+#include <math.h>
+
+MQ135Sensor::MQ135Sensor(int pin, float loadResistance, int numSamples)
+    : _pin(pin), _rl(loadResistance), _numSamples(numSamples), _r0(0.0) {}
+
+void MQ135Sensor::begin() {
+    Serial.begin(115200);
+    Serial.println("MQ135 Sensor on ESP32 - Calibrating R0");
+
+    delay(10000);  // Sensor warm-up
+    _r0 = calibrateR0();
+    Serial.print("Calibrated R0 (in clean air): ");
+    Serial.println(_r0);
+}
+
+float MQ135Sensor::calibrateR0() {
+    float analogAvg = getAverageAnalog();
+    float rs = calculateRs(analogAvg);
+    return rs;
+}
+
+float MQ135Sensor::readPPM() {
+    float analogAvg = getAverageAnalog();
+    float rs = calculateRs(analogAvg);
+    float ppm = getPPM(rs, _r0);
+
+    Serial.print("Analog Avg: ");
+    Serial.print(analogAvg);
+    Serial.print(" | Rs: ");
+    Serial.print(rs);
+    Serial.print(" kΩ | PPM: ");
+    Serial.println(ppm);
+
+    return ppm;
+}
+
+float MQ135Sensor::getAverageAnalog() {
+    long sum = 0;
+    for (int i = 0; i < _numSamples; i++) {
+        sum += analogRead(_pin);
+        delay(10);
+    }
+    return (float)sum / _numSamples;
+}
+
+float MQ135Sensor::calculateRs(float analogValue) {
+    float voltage = analogValue * (3.3 / 4095.0);  // 12-bit ADC
+    return (3.3 - voltage) * _rl / voltage;
+}
+
+float MQ135Sensor::getPPM(float rs, float r0) {
+    float ratio = rs / r0;
+    float a = -2.769;
+    float b = 2.602;
+    float log_ppm = a * log10(ratio) + b;
+    return pow(10, log_ppm);
+}
