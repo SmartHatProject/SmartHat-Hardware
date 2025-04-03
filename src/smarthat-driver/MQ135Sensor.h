183fc81a2dc8e747e0b0cce4731dd8fd62b14570
4050hop <147534729+4050hop@users.noreply.github.com>
2025-04-03 17:40:39 -0400

    Add files via upload

---
+++ src/smarthat-driver/MQ135Sensor.h
@@ -0,0 +1,22 @@
+#ifndef MQ135SENSOR_H
+#define MQ135SENSOR_H
+
+class MQ135Sensor {
+public:
+    MQ135Sensor(int pin, float loadResistance, int numSamples);
+    void begin();
+    float calibrateR0();
+    float readPPM();
+
+private:
+    int _pin;
+    float _rl;
+    int _numSamples;
+    float _r0;
+
+    float getAverageAnalog();
+    float calculateRs(float analogValue);
+    float getPPM(float rs, float r0);
+};
+
+#endif
