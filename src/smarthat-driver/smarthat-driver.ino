01972afaa7a610d93e5ac5bec1d149c759918f0d
oldgrandma101 <adphelps8@gmail.com>
2025-04-08 11:31:50 -0400

    Dust sensor code only

--- src/smarthat-driver/smarthat-driver.ino
+++ src/smarthat-driver/smarthat-driver.ino
@@ -28,23 +28,28 @@ const unsigned long UPDATE_INTERVAL = 1000;
 BleHandler bleHandler;
 
 //NoiseSensor noiseSensor(sensorPin, loudnessThreshold, numSamples);
-NoiseSensor noiseSensor(NOISE_SENSOR_PIN, NOISE_CALIBRATION_DB, NOISE_PEAK_REF, NOISE_AVG_REF);
+// NoiseSensor noiseSensor(NOISE_SENSOR_PIN, NOISE_CALIBRATION_DB, NOISE_PEAK_REF, NOISE_AVG_REF);
 
 //DustSensor dustSensor(SENSOR_PIN, LED_PIN, ADC_MAX, VCC)
 DustSensor dustSensor(DUST_SENSOR_PIN, DUST_LED_PIN, DUST_ADC_MAX, DUST_VCC);
 
 void setup() {
     Serial.begin(115200);
+    delay(1000);
+    Serial.println("\nset Serial");
     Serial.println("\n========== SmartHat Starting Up ==========");
 
     // SerialBT.begin("SmartHat");
-    bleHandler.setUpBle();
+    // bleHandler.setUpBle();
     
     //Init noise sensor
-    noiseSensor.begin();
+    // noiseSensor.begin();
 
     //init dust sensir
+    Serial.println("\nsetting Dust Sensor");
     dustSensor.setUpDustSensor();
+    Serial.println("\nsuccessfully set up dust sensor");
+
 
     analogReadResolution(12); // Set the resolution to 12 bits (0-4095)
 
@@ -53,40 +58,45 @@ void setup() {
 
 void loop() {
 
-    bool currentConnectionStatus = bleHandler.isDeviceConnected();
+    // bool currentConnectionStatus = bleHandler.isDeviceConnected();
 
-    //print connection status for new connection or disconnection
-    if (currentConnectionStatus != lastConnectionStatus){
-      if (currentConnectionStatus){
-        Serial.println("\n========== CONNECTED ==========");
-      }
-      else{
-        Serial.println("\n========== BLE CLIENT DISCONNECTED ==========");
-      }
-    }
-    lastConnectionStatus = currentConnectionStatus;
+    // //print connection status for new connection or disconnection
+    // if (currentConnectionStatus != lastConnectionStatus){
+    //   if (currentConnectionStatus){
+    //     Serial.println("\n========== CONNECTED ==========");
+    //   }
+    //   else{
+    //     Serial.println("\n========== BLE CLIENT DISCONNECTED ==========");
+    //   }
+    // }
+    // lastConnectionStatus = currentConnectionStatus;
 
     
 
-    //update BLE characteristics if it's been more than 1000 ms and the SmartHat is connected to a client
-    unsigned long currentTime = millis();
-    if (currentTime - lastUpdateTime >= UPDATE_INTERVAL){
+    // //update BLE characteristics if it's been more than 1000 ms and the SmartHat is connected to a client
+    // unsigned long currentTime = millis();
+    // if (currentTime - lastUpdateTime >= UPDATE_INTERVAL){
 
-      lastUpdateTime = currentTime;
+    //   lastUpdateTime = currentTime;
 
-      if(currentConnectionStatus){  
+    //   if(currentConnectionStatus){  
 
-        // update value of dust characteristic with new sensor reading
-        float dustSensorReading = dustSensor.readDustSensor();
-        bleHandler.updateDustLevel(dustSensorReading);
+    //     // update value of dust characteristic with new sensor reading
+    //     float dustSensorReading = dustSensor.readDustSensor();
+    //     bleHandler.updateDustLevel(dustSensorReading);
     
-        // update value of sound characteristic with new sensor reading
-        noiseSensor.update();
-        bleHandler.updateSoundLevel(noiseSensor.getPeakDB());
-      }
+    //     // update value of sound characteristic with new sensor reading
+    //     noiseSensor.update();
+    //     bleHandler.updateSoundLevel(noiseSensor.getPeakDB());
+    //   }
     
-    }
-    yield();
+    // }
+    // yield();
+  float dustSensorReading = dustSensor.readDustSensor();
+  Serial.println("\n Dust sensor reading: ");
+  Serial.println(dustSensorReading);
+
+
     delay(1000);
 }
 
